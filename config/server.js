46fcd5f56ac076ccbcc89f9c296a307808267518
John Pecson <jonpecson.io@gmail.com>
2021-04-08 09:53:44 +0800

    update fallback port

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  port: env.int('PORT', 80),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '387b202d9d71b075096d0ebcf0945fae'),
