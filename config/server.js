128ad96a34b25f38dd461a096d91f625a1548d54
John Pecson <jonpecson.io@gmail.com>
2021-04-08 09:33:54 +0800

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '387b202d9d71b075096d0ebcf0945fae'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'ae7418c4-5ade-40ab-9151-65b410ef57cd'
+};
